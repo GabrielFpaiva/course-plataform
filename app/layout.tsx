a6dd2d4678c6687e39da848741ddb70c82f9c1b6
GabrielFPaiva <gabrielpurse006@gmail.com>
2025-03-14 15:19:24 -0300

    deploy

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ const inter = Inter({ subsets: ["latin"] })
 export const metadata: Metadata = {
   title: "CoursePlatform",
   description: "Learn from the best courses online",
-    generator: 'g7.dev'
+    generator: 'v0.dev'
 }
 
 export default function RootLayout({
